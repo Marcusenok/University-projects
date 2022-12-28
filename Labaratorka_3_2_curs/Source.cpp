88d1904e15db4f32319bf09ed2bdbe61c233f3b2
Marcusenok <74916064+Marcusenok@users.noreply.github.com>
2022-12-28 18:35:32 +0300

    Add files via upload

--- Labaratorka_3_2_curs/Source.cpp
+++ Labaratorka_3_2_curs/Source.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include"Dynamic_bidirectional_list.h"
+#include"DynamicList.h"
 
 using namespace std;
 
@@ -8,5 +8,22 @@ int main()
 	system("chcp 1251");
 	system("cls");
 
+	DynamicList<int> List(100);
+	DynamicList<double> List1(100.123);
 
+	List + 11;
+	List + 13;
+	List1 + 3.2;
+	List1 + 4.5;
+
+	DynamicList<double> List2 = List1;
+
+	List2 + 6.66;
+	List2 + 7.77;
+
+	List.Print();
+	List1.Print();
+	List2.Print();
+
+	return 0;
 }
